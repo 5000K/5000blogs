d9e2907c3744732b64c0c00926cceba0a56be68a
bwt <thebluewingtitan@gmail.com>
2026-03-02 20:24:03 +0100

    add initial version of the parser

---
+++ go.mod
@@ -0,0 +1,8 @@
+module 5000blogs
+
+go 1.26.0
+
+require (
+	github.com/go-chi/chi/v5 v5.2.5
+	gopkg.in/yaml.v3 v3.0.1
+)
---
+++ incoming/server.go
@@ -0,0 +1,44 @@
+package incoming
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func Serve() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, "public"))
+	FileServer(r, "/static", filesDir)
+
+	_ = http.ListenAndServe(":3000", r)
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
+}
