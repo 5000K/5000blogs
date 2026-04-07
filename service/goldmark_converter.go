050d9441b6e4ac773470999ebc1f2c0e32f940ce
kicher-erbse <contact@bonuswings.com>
2026-04-07 10:47:45 +0200

    add import

--- service/goldmark_converter.go
+++ service/goldmark_converter.go
@@ -13,6 +13,7 @@ import (
 	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer"
+	"github.com/yuin/goldmark/renderer/html"
 	"github.com/yuin/goldmark/text"
 	"github.com/yuin/goldmark/util"
 )
